976500b7dbb1790b8ea2313439ab6f475abcfa85
文刀水寿 <18700222185@163.com>
2025-05-14 23:07:18 +0800

    5-14-1

--- Django_LoveSync_Diary/code/DjangoPro1/App/views.py
+++ Django_LoveSync_Diary/code/DjangoPro1/App/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render, HttpResponse, reverse
 # 首页
 async def user_index(requeust):
     return HttpResponse(requeust, 'index.html')
+
+
+# 登录
+async def user_login(requset):
+    if requset.method == 'GET':
+        return HttpResponse(requset, 'login.html')
+
+
+# 注册
+async def user_register(request):
+    if request.method == 'GET':
+        return HttpResponse(request, 'register.html')
--- Django_LoveSync_Diary/code/DjangoPro1/DjangoPro1/urls.py
+++ Django_LoveSync_Diary/code/DjangoPro1/DjangoPro1/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     # 首页
     path('/', user_index),
     path('index/', user_index, name='index'),
+    # 登录
+    path('login/', user_login, name='login'),
+
+    # 注册
+    path('register/', user_register, name='register'),
 
     path('admin/', admin.site.urls),
 ]
